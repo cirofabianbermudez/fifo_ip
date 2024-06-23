e948b1d88f1861b6c2dbf6dc2d9f9d7b80ef4406
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-06-24 00:44:51 +0200

    feat: add fifo_if.sv

---
+++ tb/fifo_if.sv
@@ -0,0 +1,28 @@
+interface fifo_if #(
+    parameter int WordLength = 8
+) (
+    input logic clk_i
+);
+
+  logic                  rst_i;
+  logic                  rd_i;
+  logic                  wr_i;
+  logic [WordLength-1:0] w_data_i;
+  logic [WordLength-1:0] r_data_o;
+  logic                  empty_o;
+  logic                  full_o;
+
+  clocking cb @(posedge clk);
+    default input #1ns output #1ns;
+    output rst_i;
+    output rd_i;
+    output wr_i;
+    output w_data_i;
+    input  r_data_o;
+    input  empty_o;
+    input  full_o;
+  endclocking
+
+  modport dvr (clocking cb, output rst_i);
+
+endinterface : fifo_if
