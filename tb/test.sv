cf81292f585dcf0bb45ca23805ca917fcd6521b7
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-06-25 22:40:15 +0200

    feat: add test sequence

--- tb/test.sv
+++ tb/test.sv
@@ -5,42 +5,51 @@ module test (
   initial begin
     $display("Begin Of Simulation.");
     reset();
-    normal();
-    bounce();
+    write('d0);   // One write
+    write('d10);  // Three writes
+    write('d20);
+    write('d30);
+    read();       // One read
+    write('d40);  // Four writes
+    write('d50);
+    write('d60);
+    write('d70);
+    write('d80);  // One more write FULL
+    read();       // Two reads
+    read();
+    read();       // Five reads
+    read();
+    read();
+    read();
+    read();
+    read();       // One more read EMPTY
     $display("End Of Simulation.");
     $finish;
   end
 
   task reset();
-    db_if.rst = 1'b1;
-    db_if.cb.sw_i <= 1'b0;
-    repeat (2) @(db_if.cb);
-    db_if.cb.rst <= 1'b0;
-    repeat (12) @(db_if.cb);
+    fifo_if.rst_i = 1'b1;
+    fifo_if.cb.rd_i     <= 1'b0;
+    fifo_if.cb.wr_i     <= 1'b0;
+    fifo_if.cb.w_data_i <= 'd0;
+    repeat (1) @(fifo_if.cb);
+    fifo_if.cb.rst_i <= 1'b0;
+    repeat (1) @(fifo_if.cb);
   endtask : reset
 
-  task normal();
-    db_if.cb.sw_i <= 1'b1;
-    repeat (30) @(db_if.cb);
-    db_if.cb.sw_i <= 1'b0;
-    repeat (30) @(db_if.cb);
-  endtask : normal
+  task write(input logic [7:0] value);
+    fifo_if.cb.w_data_i <= value;
+    fifo_if.cb.wr_i     <= 1'b1;
+    @(fifo_if.cb);
+    fifo_if.cb.wr_i <= 1'b0;
+    repeat (1) @(fifo_if.cb);
+  endtask : write
 
-  task bounce();
-    int delay1, delay2;
-    realtime time1, time2;
-    for (int i = 0; i < 50; i++) begin
-      db_if.cb.sw_i <= 1'b1;
-      time1  = $realtime;
-      delay1 = $urandom_range(1, 15);
-      repeat (delay1) @(db_if.cb);
-      db_if.cb.sw_i <= 1'b0;
-      delay2 = $urandom_range(1, 15);
-      time2  = $realtime;
-      repeat (delay2) @(db_if.cb);
-      $display("iter = %3d, delay1 = %3d, delay2 = %3d, time1 = %t, time2 = %t", i, delay1, delay2,
-               time1, time2);
-    end
-  endtask : bounce
+  task read();
+    fifo_if.cb.rd_i <= 1'b1;
+    @(fifo_if.cb);
+    fifo_if.cb.rd_i <= 1'b0;
+    repeat (1) @(fifo_if.cb);
+  endtask : read
 
 endmodule : test
